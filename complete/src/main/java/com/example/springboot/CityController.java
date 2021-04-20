69a9a26b42a21ce209e7df5410fd9cb24b5b78f5
adrian.haz <adrian.haz@udc.es>
2021-04-20 17:13:07 +0200

    Realizado issue de creación del endpoint /cities

--- complete/src/main/java/com/example/springboot/CityController.java
+++ complete/src/main/java/com/example/springboot/CityController.java
@@ -3,12 +3,14 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.springboot.City;
+
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
+	@RequestMapping("/cities")
+	public City[] cities() {
+		return cities;
 	}
 
 }
